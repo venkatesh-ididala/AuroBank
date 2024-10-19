71c29234484de34bf1afea93da3de0409dca692b
venkatesh-ididala <venkateshididala@gmail.com>
2024-10-19 21:03:02 +0530

    implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalancebox from '@/components/TotalBalancebox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
+
+    const loggedIn={firstName: 'Venkatesh',lastName:'Ididala'};
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                type="greeting" 
+                title="welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transactions effeciently." />
+               
+                <TotalBalancebox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1258.34}
+
+                />
+                 </header> 
+
+                 RECENT TRANSACTIONS
+                 <RightSidebar 
+                 user={loggedIn}
+                 transactions={[]}
+                 banks={[{},{}]} />
+                </div>
+    </section>
   );
 };
 
